8ad17bd7b6a31f6a1dfb0373b425923352ec2c59
Saad Zaoual <saadzaoual@gmail.com>
2026-08-06 16:01:14 +0100

    mariadb / wp : DONE

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -16,7 +16,9 @@ if [ ! -d /var/lib/mysql/mysql ]; then
 	mysql -u root -e "create database if not exists \`${MYSQL_DATABASE}\`;"
 	mysql -u root -e "create user if not exists \`${MYSQL_USER}\`@'%' identified by '$(cat /run/secrets/db_password)';"
 	mysql -u root -e "grant all privileges on \`${MYSQL_DATABASE}\`.* to \`${MYSQL_USER}\`@'%';"
+	mysql -u root -e "delete from mysql.user where User='';"
 	mysql -u root -e "alter user 'root'@'localhost' identified by '$(cat /run/secrets/db_root_password)';"
+	mysql -u root -e "flush privileges;"
 
 	mysqladmin -u root -p"$(cat /run/secrets/db_root_password)" shutdown
 fi
